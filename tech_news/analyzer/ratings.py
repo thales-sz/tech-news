5eaa85ef68d988e103659ccdba8c82fdc90a6437
Thales <thales.souz@outlook.com>
2023-02-09 17:48:21 -0300

    feat: Counter para top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,9 @@
+from collections import Counter
 from tech_news.database import find_news
+
 
 # Requisito 10
 def top_5_categories():
-    
+    news = find_news()
+    teste = Counter([new["category"] for new in news])
+    print(teste)
