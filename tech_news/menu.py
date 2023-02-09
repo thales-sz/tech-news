2628ee36bdf684b34464cd2ace934d61ada6dd35
Thales <thales.souz@outlook.com>
2023-02-09 18:06:32 -0300

    feat: input inicial finalizado, req 11

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -6,12 +6,12 @@ def analyzer_menu():
     try:
         option = input(
             """Selecione uma das opções a seguir:\n"""
-            """0 - Popular o banco com notícias;\n"""
-            """1 - Buscar notícias por título;\n"""
-            """2 - Buscar notícias por data;\n"""
-            """3 - Buscar notícias por categoria;\n"""
-            """4 - Listar top 5 categorias;\n"""
-            """5 - Sair."""
+            """ 0 - Popular o banco com notícias;\n"""
+            """ 1 - Buscar notícias por título;\n"""
+            """ 2 - Buscar notícias por data;\n"""
+            """ 3 - Buscar notícias por categoria;\n"""
+            """ 4 - Listar top 5 categorias;\n"""
+            """ 5 - Sair.\n"""
         )
         print(option)
     except (IndexError, ValueError, TypeError):
