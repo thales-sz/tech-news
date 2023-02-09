3336b1bf4bd9a8d6615bf46e7e7a9c23f339f016
Thales <thales.souz@outlook.com>
2023-02-09 18:02:23 -0300

    feat: inciando req 11

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,18 @@
+import sys
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    try:
+        option = input(
+            """Selecione uma das opções a seguir:
+            0 - Popular o banco com notícias;
+            1 - Buscar notícias por título;
+            2 - Buscar notícias por data;
+            3 - Buscar notícias por categoria;
+            4  - Listar top 5 categorias;
+            5 - Sair."""
+        )
+        print(option)
+    except (IndexError, ValueError, TypeError):
+        print("Opção inválida", sys.stderr)
