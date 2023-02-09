2a0af45cf3f83eef5d6bc9e213b2777b6a8347db
Thales <thales.souz@outlook.com>
2023-02-09 17:58:38 -0300

    feat: função top_5_categories, req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -4,6 +4,11 @@ from tech_news.database import find_news
 
 # Requisito 10
 def top_5_categories():
+    result = []
     news = find_news()
-    teste = Counter([new["category"] for new in news])
-    print(teste)
+    categories_list = Counter([new["category"] for new in news])
+    for cate in sorted(
+        categories_list.items(), key=lambda item: (-item[1], item[0])
+    ):
+        result.append(cate[0])
+    return result
