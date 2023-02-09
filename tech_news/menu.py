01f7f5b44877e30272130306bc75364f2095fe6d
Thales <thales.souz@outlook.com>
2023-02-09 18:43:56 -0300

    feat: função para Opção 0 de input

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,8 +1,26 @@
 import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import search_by_category, search_by_date
 
 
 # Requisitos 11 e 12
 def analyzer_menu():
+    def option00():
+        value = input("Digite a quantidade de noticias: ")
+        return get_tech_news(int(value))
+
+    def option01():
+        value = input("Digite a quantidade de noticias: ")
+        return get_tech_news(int(value))
+
+    switch = {
+        "0": option00,
+        "1": option01,
+        "2": "Opção 2",
+        "3": "Opção 3",
+        "4": "Opção 4",
+        "5": "Opção 5",
+    }
     try:
         option = input(
             """Selecione uma das opções a seguir:\n"""
@@ -14,5 +32,6 @@ def analyzer_menu():
             """ 5 - Sair.\n"""
         )
         print(option)
+        switch.get(option)
     except (IndexError, ValueError, TypeError):
         print("Opção inválida", sys.stderr)
